68160e4211cc71e03490853f89105dc2d75f1b3c
Matthias Blaschke <matthias.blaschke@physik.uni-augsburg.de>
2024-04-18 10:19:05 +0200

    Remove nohup

--- utils/create_session.sh
+++ utils/create_session.sh
@@ -7,7 +7,7 @@ mkdir ~/$host_name
 cd ~/$host_name
 git clone https://github.com/blaschma/Girls_Day_2024
 cd Girls_Day_2024
-nohup jupyter notebook --no-browser --NotebookApp.token='' --NotebookApp.password='' &
+jupyter notebook --no-browser --NotebookApp.token='' --NotebookApp.password='' &
 profile="gast_$host_name"
 firefox-esr -CreateProfile $profile
 firefox-esr --kiosk "http://localhost:8888/" -P $profile
